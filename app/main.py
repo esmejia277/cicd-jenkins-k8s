19d2597beb85b019b3b6c3e533afb33f74dfefb1
Esteban Mejia <estebanmejia277@gmail.com>
2025-09-30 23:56:00 -0500

    Testing pipeline

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hi Snake!! Using my CICD with Jenkins"}
+    return {"message": "Hi Snake!! Using my CICD with Jenkins, testing pipeline"}
